337f971accd83978b7c02e65b7c73b4f734adaea
Luiz Leandrini <guilhermeleandrini@hotmail.com>
2020-11-10 14:15:30 -0300

    Service layer, component registration

---
+++ src/main/java/com/example/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.example.course.services;
+
+import java.security.cert.PKIXRevocationChecker.Option;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.course.entities.User;
+import com.example.course.respositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		
+		return repository.findAll();
+	}
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
