dfb16867f66ebb49cb370a8902e643e822ca7c7d
Luiz Leandrini <guilhermeleandrini@hotmail.com>
2020-11-10 10:31:19 -0300

    User Entity and Resource

---
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.example.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Luiz", "sdgak@oskdpg.com", "999999", "123");
+		return ResponseEntity.ok().body(u);
+	}
+}
